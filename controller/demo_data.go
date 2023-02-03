347a77ae3a3bd6fe8c4d4d3f35decf6edf7877f4
ikuraoo <1294429919@qq.com>
2023-02-03 23:23:14 +0800

    访问本地public视频

--- controller/demo_data.go
+++ controller/demo_data.go
@@ -2,9 +2,10 @@ package controller
 
 var DemoVideos = []Video{
 	{
-		Id:            1,
-		Author:        DemoUser,
-		PlayUrl:       "https://www.w3schools.com/html/movie.mp4",
+		Id:     1,
+		Author: DemoUser,
+		//PlayUrl: "https://www.w3schools.com/html/movie.mp4",
+		PlayUrl:       "http://192.168.1.6:8080/static/bear.mp4",
 		CoverUrl:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
 		FavoriteCount: 0,
 		CommentCount:  0,
